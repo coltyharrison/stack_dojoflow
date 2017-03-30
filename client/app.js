ddbc8e7eea13f1e78bb184fdbadaed7e2e5b70b8
Taylor Krusen <taykrusen@gmail.com>
2017-03-29 23:20:36 -0700

    html entries from database working on question page

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', ['ngRoute', 'ui.tinymce']);
 app.config(function($routeProvider) {
     $routeProvider
     .when('/', {
