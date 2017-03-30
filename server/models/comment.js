46d7e3fd81e7dbf7cf939b0bbf39f9164460d389
Colty Harrison <coltyharrison@gmail.com>
2017-03-30 10:57:33 -0700

    fixed question->answer->comment->user schmea

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,10 +1,8 @@
 var mongoose = require('mongoose'),
+    UserSchema = require('./user.js'),
     CommentSchema = mongoose.Schema({
         comment: String,
-        _user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
-        },
+        _user: UserSchema,
         _answer: {type: mongoose.Schema.Types.ObjectId, ref: 'Answer'},
         _question: {type: mongoose.Schema.Types.ObjectId, ref: 'Question'},
         upvotes: {type: Number, default: 0},
